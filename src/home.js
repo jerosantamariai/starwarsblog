7c5fc27aacdc3f9dc5cc115572629db8c46c4947
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-03-24 20:49:51 -0300

    preparing to divide in function components

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = props => {
     return (
         <>
+
             <div className="header">
                 <div className="container">
                     <div className="row" id="social">
@@ -31,13 +32,13 @@ const Home = props => {
                             <div className="row">
                                 <div className="col-6"></div>
                                 <div className="col-6">
-                                <p className="userpath"><a className="login" href="/#">LOG IN</a>    /  /     <a className="signup" href="/#">SIGN UP</a></p>
+                                    <p className="userpath"><a className="login" href="/#">LOG IN</a>      /    /       <a className="signup" href="/#">SIGN UP</a></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="navbar">
+                <div className="navbar navbar-fixed-top">
                     <div className="row" id="centerbar">
                         <div className="col-12 d-flex justify-content-center">
                             <nav className="nav">
@@ -55,7 +56,99 @@ const Home = props => {
                 </div>
             </div>
             <div className="info">
-
+                <div className="databanksearch">
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-10">
+                            <div className="input-group input-group-lg">
+                                <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="SEARCH DATABANK" />
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary" type="button"><i className="fa fa-search"></i>Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr width="1050px" height="5px" color="gray"></hr>
+                <h6 className="firsttitle">STAR WARS: THE RISE OF SKYWALKER //</h6>
+                <hr width="1050px" height="5px" color="gray"></hr>
+                <div className="bloginfo">
+                    <div className="row row-cols-1 row-cols-md-4">
+                        <div className="col mb-4">
+                            <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                        <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                        <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row row-cols-1 row-cols-md-4">
+                        <div className="col mb-4">
+                            <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                        <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card">
+                                <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
