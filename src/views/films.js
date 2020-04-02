f1f9d9876b3b90ff7ca6d55bf32a3f3303f77484
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-04-01 21:31:45 -0300

    add buttons to show more filmS... API isnt updated so theres missing information

--- src/views/films.js
+++ src/views/films.js
@@ -38,6 +38,38 @@ const Films = props => {
                         }
                     </div>
                 </div>
+                <div className="row pb-5" id="moreinfo">
+                    <div className="col-md-6 offset-3 d-flex justify-content-between">
+                        {
+                            !!store.films &&
+                                store.films.previous !== null ?
+                                (
+                                    <button className="btn btn-dark btn-md"
+                                        onClick={() => actions.getFilms(store.films.previous)}>
+                                        <i class="fa fa-arrow-left"></i> Previous
+                                    </button>
+                                ) : (
+                                    <span className="btn btn-dark btn-md disabled">
+                                        <i class="fa fa-arrow-left"></i> Previous
+                                    </span>
+                                )
+                        }
+                        {
+                            !!store.films &&
+                                store.films.next !== null ?
+                                (
+                                    <button className="btn btn-dark btn-md"
+                                        onClick={() => actions.getFilms(store.films.next)}>
+                                        Next <i class="fa fa-arrow-right"></i>
+                                    </button>
+                                ) : (
+                                    <span className="btn btn-dark btn-md disabled">
+                                        Next <i class="fa fa-arrow-right"></i>
+                                    </span>
+                                )
+                        }
+                    </div>
+                </div>
             </div>
         </>
     )
