a1a5943064c22927509eb35ad868fdbb086cc8ce
Jerónimo Santa María Illanes <jerosantamariai@MacBook-Pro-de-Jeronimo.local>
2020-10-19 22:52:12 -0300

    anakin decides to fight with obiwan

--- src/components/collage.js
+++ src/components/collage.js
@@ -122,7 +122,7 @@ export default [
     "https://i.pinimg.com/originals/a4/b2/3d/a4b23dc98c0f2ff1269017c60a24eb28.jpg",
     "https://64.media.tumblr.com/442513caac35229ccdd5e39fe822d6bf/f5c2981514e757fd-01/s500x750/a644d95d7d0936a19d19c5737e071711edfb489c.gifv",
     "https://images.g2a.com/newlayout/323x433/1x1x0/bf3fe4abb5ee/5bf530c0ae653aac3a13c842",
-    // "",
+    "https://media1.tenor.com/images/c8315a097a28544086502450242c38f5/tenor.gif?itemid=18489728",
     // "",
     // "",
     // "",
